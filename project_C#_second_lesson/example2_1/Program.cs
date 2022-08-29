17bfeea9f49aab8284e5e12dd9fd3cfa2a480579
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 01:13:10 +0300

    add example2_1

---
+++ project_C#_second_lesson/example2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+int a = num / 10;
+int b = num % 10;
+
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+Console.WriteLine(max);
