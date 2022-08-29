c61328f317bfcef1e94c050e37e24f79c9a90742
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 02:02:05 +0300

    add example2_3

---
+++ project_C#_second_lesson/example2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+// Console.WriteLine($"{num1};{num2}"); 
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 % num2 == 0)
+{ 
+    Console.WriteLine($"Число {num1} кратно числу {num2}"); 
+}
+else
+{
+    Console.WriteLine($"Остаток от деления числа {num1} на {num2} равняется {num1 % num2}");
+}
