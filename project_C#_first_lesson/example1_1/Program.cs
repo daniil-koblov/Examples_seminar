48e081e0990185bffcf0f4eb23b888e2c768c6c1
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-29 19:50:57 +0300

    last add fo first lesson

--- project_C#_first_lesson/example1_1/Program.cs
+++ project_C#_first_lesson/example1_1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int result = num1 * num1;
+Console.WriteLine(result);
