fe8580df271a82c315a4563819a43923a62b2038
Daniil Koblov <1koblov.danil1@gmail.com>
2022-08-30 02:44:03 +0300

    add example2_5

--- project_C#_second_lesson/example2_5/Program.cs
+++ project_C#_second_lesson/example2_5/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+    Console.WriteLine($"Число {num2} является квадратом числа {num1}");
+}
+else if (num2 * num2 == num1)
+{
+    Console.WriteLine($"Число {num1} является квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1}  не является квадратом числа {num2}");
+    Console.WriteLine($"Число {num2}  не является квадратом числа {num1}");
+}
+
+
+
+// if (num2 * num2 == num1)
+// {
+//     Console.WriteLine($"Число {num1} является квадратом числа {num2}");  
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num1}  не является квадратом числа {num2}");
+// }
+
