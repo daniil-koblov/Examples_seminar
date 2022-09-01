55a3b1d071a0ce89f1da5b297dc4147f122e9b62
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-01 23:56:04 +0300

    add example3_2

---
+++ project_C#_third_lesson/example3_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num > 0) && (num < 5))
+{
+    if (num == 1)
+    {
+      Console.Write("Диапазон координат x > 0 y > 0");  
+    }
+    else if (num == 2)
+    {
+      Console.Write("Диапазон координат x > 0 y < 0");  
+    }
+    else if (num == 3)
+    {
+      Console.Write("Диапазон координат x < 0 y < 0");  
+    }
+    else
+    {
+      Console.Write("Диапазон координат x < 0 y > 0");  
+    }
+}
+else
+{
+    Console.Write("Вы ввели не правильный номер четверти (нужно от 1 до 4)");
+}
