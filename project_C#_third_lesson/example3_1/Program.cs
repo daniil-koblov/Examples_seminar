a62402f3b70128c840c9c5087a210c2c0bcde2a9
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-01 23:34:02 +0300

    add example3_1

---
+++ project_C#_third_lesson/example3_1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.Write("Введите координаты точки");
+Console.Write("Введите х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите у: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x == 0) || (y == 0))
+{
+    Console.Write("Координаты не должны равнятся нулю.");
+}
+else if ((x > 0) && (y > 0))
+{
+    Console.Write("Точка в первой четверти.");
+}
+else if ((x > 0) && (y < 0))
+{
+    Console.Write("Точка во второй четверти.");
+}
+else if ((x < 0) && (y < 0))
+{
+    Console.Write("Точка в третьей четверти.");
+}
+else
+{
+    Console.Write("Точка в четвертой четверти.");
+}
