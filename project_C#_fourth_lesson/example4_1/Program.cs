331bac66ab895ba26a177564be414ed47c2273b1
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-02 20:44:17 +0300

    add example4_1

---
+++ project_C#_fourth_lesson/example4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает число А и выдает сумму от чисел от 1 до А.
+Console.Clear();
+void SumOneToA(int a)
+{
+    int sum = 0;
+    for(int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+SumOneToA(num1);
+
+Console.Write("Введите число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+SumOneToA(num2);
+
+Console.Write("Введите число C: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+SumOneToA(num3);
