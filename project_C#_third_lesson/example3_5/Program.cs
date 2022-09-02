12c7e539a50308c2d6cb5e043519843719d226f0
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-02 03:32:01 +0300

    add example3_5

---
+++ project_C#_third_lesson/example3_5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите многозначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = 10;
+bool isTrue = true;
+while(num > 0)
+{
+    if(temp < num % 10)
+    {   
+        isTrue = false;
+    }
+    temp = num % 10;
+    num /= 10;
+}
+if(isTrue)
+Console.WriteLine("Число упорядоченное");
+else
+Console.WriteLine("Число неупорядоченное");
