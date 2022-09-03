f55b0e7d70a7dbbffede258ecc3d2c91f737720d
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-03 20:06:47 +0300

    add example4_2

---
+++ project_C#_fourth_lesson/example4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+void Sum(int a)
+{
+    string num = Convert.ToString(a);
+    int len = num.Length;
+    Console.WriteLine(len);
+}
+
+Console.WriteLine("Введите целое число А: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Sum(num1);
+
+Console.WriteLine("Введите целое число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Sum(num2);
+
+Console.WriteLine("Введите целое число C: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Sum(num3);
