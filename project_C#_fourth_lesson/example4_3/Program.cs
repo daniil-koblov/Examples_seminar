d25e8157e2c4d1a7e8e5aec226daf744e1aed2c0
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-05 14:32:40 +0300

    add example4_3

--- project_C#_fourth_lesson/example4_3/Program.cs
+++ project_C#_fourth_lesson/example4_3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void Mult(int a)
+{
+    if(a > 0)
+    {
+        int sum = 1;
+        for(int i = 1; i <= a; i++)
+        {
+            sum = sum * i;
+        }
+        Console.WriteLine(sum);
+    }
+    else if(a == 0)
+    {
+        a = 1;
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели число неверно");
+    }
+   
+}       
+
+Console.WriteLine("Введите целое число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Mult(A);
+
