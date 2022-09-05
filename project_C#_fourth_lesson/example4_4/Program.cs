d9b3c29257f23c648de11cb2e392e073e9bb17f1
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-05 16:20:33 +0300

    add example4_4

---
+++ project_C#_fourth_lesson/example4_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+void FillArray(int[] a)
+{
+    int length = a.Length;
+    if(length > 0)
+    {
+        for(int i = 0; i < a.Length; i++)
+            {
+                int random = new Random().Next(0, 2); 
+                a[i] = random;
+                Console.Write($"{a[i]} ");
+            }
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("Длинна должна быть равна 1 или больше.");
+    }
+}
+
+Console.WriteLine("Введите длинну массива");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [len];
+FillArray(array);
