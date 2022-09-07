aedd8ffca3822cc2074497970caf99a09663946e
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-07 12:56:45 +0300

    add example5_1

---
+++ project_C#_fifth_lesson/example5_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+string str1 = "Hello";
+string str2 = "world";
+
+// string str3 = string.Concat(str1, str2, "####");
+
+int[] values = new int[] {1, 2, 3, 4, 5};
+string str3 = string.Join("*", values);
+
+Console.WriteLine(str3);
