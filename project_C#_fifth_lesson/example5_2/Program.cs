9fce08f71be4e6c63031929bb5f5eb7c4c7e3843
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-07 14:28:55 +0300

    add example5_2

---
+++ project_C#_fifth_lesson/example5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int[] arrayRandom = new int[12];
+
+for(int i = 0; i < arrayRandom.Length; i++)
+{
+    arrayRandom[i] = new Random().Next(-10, 10);
+    Console.Write($"{arrayRandom[i]} ");
+}
+
+int sumPositive = 0;
+int sumNegative = 0;
+for(int i = 0; i < arrayRandom.Length; i++)
+{
+    if(arrayRandom[i] >= 0)
+        sumPositive += arrayRandom[i];
+    else
+        sumNegative += arrayRandom[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных цифр равна {sumPositive}, а отрицательных {sumNegative}");
