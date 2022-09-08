18306bd642203d5d0da50381826445de55742a24
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-08 16:28:38 +0300

    add example5_6

--- project_C#_fifth_lesson/example5_6/Program.cs
+++ project_C#_fifth_lesson/example5_6/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+void FillArray(int[] a)
+{
+    for(int i = 0; i < a.Length; i++)
+    {
+        int random = new Random().Next(1, 124);
+        a[i] = random;
+        Console.Write($"{a[i]} ");
+    }
+    Console.WriteLine();
+    for(int i = 0; i < a.Length / 2; i++)
+    Console.Write($"{a[i] * a[a.Length - i - 1]} ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите длинну массива");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [len];
+FillArray(array);
