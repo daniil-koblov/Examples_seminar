6af8c5e46b3f96592566fdbd0a6552d76f6ed7a1
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-09 20:08:17 +0300

    add example6_1

---
+++ project_C#_sixth_lesson/example6_1/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+/* int [,] arr = new int [3,4];
+
+arr.GetLength(0);
+arr.GetLength(1); */
+
+void Reverse(int[] a)
+{
+    for(int i = 0; i < a.Length; i++)
+    {
+        int random = new Random().Next(1, 100);
+        a[i] = random;
+        Console.Write($"{a[i]} ");
+    }
+    Console.WriteLine();
+    int rev = 0;
+    if(a.Length % 2 == 0)
+    {
+        for(int i = 0; i < a.Length / 2; i++)
+        {
+            rev = a[a.Length - i - 1];
+            a[a.Length - i - 1] = a[i];
+            a[i] = rev;
+        }
+        for(int i = 0; i < a.Length; i++) 
+            Console.Write($"{a[i]} ");
+    }
+    else
+    {
+        for(int i = 0; i <= a.Length / 2; i++)
+        {
+            if(i < a.Length / 2)
+            {
+                rev = a[a.Length - i - 1];
+                a[a.Length - i - 1] = a[i];
+                a[i] = rev;
+            }
+            else a[i] += 0;           
+        }
+        for(int i = 0; i < a.Length; i++)
+            Console.Write($"{a[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите длинну массива");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [len];
+Reverse(array);
