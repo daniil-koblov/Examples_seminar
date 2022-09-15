e70c1a39c3f4d7bdd3e0238d3f03023fefa5f1c6
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-15 21:50:02 +0300

    add example7_2

---
+++ project_C#_seventh_lesson/example7_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+Console.Clear();
+
+int m = new Random().Next(1, 10);
+int n = new Random().Next(1, 10);
+int[,] num = new int [m,n];
+for(int i = 0; i < num.GetLength(0); i++)
+{
+    for(int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = i + j;
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
