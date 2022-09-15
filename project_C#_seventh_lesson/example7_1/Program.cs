ed0532d5a5c47c5e2daf68a715d7fb95b393ae11
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-15 21:24:57 +0300

    add example7_1

---
+++ project_C#_seventh_lesson/example7_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+Console.Clear();
+
+int m = new Random().Next(1, 10);
+int n = new Random().Next(1, 10);
+int[,] num = new int [m,n];
+for(int i = 0; i < num.GetLength(0); i++)
+{
+    for(int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(1, 10);
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
