fa29aef0a23a02ec96ab37ead3122272d5c9b8b4
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-17 23:21:20 +0300

    second add example8_3

--- project_C#_eighth_lesson/example8_3/Program.cs
+++ project_C#_eighth_lesson/example8_3/Program.cs
@@ -2,8 +2,8 @@
 // Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
 Console.Clear();
 
-int m = new Random().Next(1, 8);
-int n = new Random().Next(1, 8);
+int m = new Random().Next(1, 4);
+int n = new Random().Next(1, 4);
 int[,] number = new int[m, n];
 void FillArray(int[,] num)
 {
@@ -41,4 +41,39 @@ for (int strings = 0; strings < number.GetLength(0); strings++)
 for(int i = 0; i < 10;  i++)
 {
     Console.WriteLine($"Количество чисел {i} : " + array[i]);
+}
+
+// 2 вариант
+Console.WriteLine();
+Console.WriteLine("2 вариант");
+
+int m2 = new Random().Next(1, 4);
+int n2 = new Random().Next(1, 4);
+int[,] num = new int[m2, n2];
+int max = 0;
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(1, 10);
+        Console.Write($"{num[i, j]} ");
+        if (num[i, j] > max) max = num[i, j];
+    }
+    Console.WriteLine();
+}
+
+for (int z = 0; z <= max; z++)
+{
+    int count = 0;
+    for (int i = 0; i < num.GetLength(0); i++)
+    {
+        for (int j = 0; j < num.GetLength(1); j++)
+        {
+            if (num[i, j] == z)
+            {
+                count++;
+            }
+        }
+    }
+    if (count != 0) Console.WriteLine($"{z} - {count} раз");
 }
