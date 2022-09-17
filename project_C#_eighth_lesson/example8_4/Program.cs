2d1cdbcf12d5c8359eca620fdc4d72b21832fab3
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-17 23:33:34 +0300

    add example8_4

--- project_C#_eighth_lesson/example8_4/Program.cs
+++ project_C#_eighth_lesson/example8_4/Program.cs
@@ -10,4 +10,21 @@
 // 9 4 2
 // 2 2 6
 // 3 4 7
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+int m = new Random().Next(1, 4);
+int n = new Random().Next(1, 4);
+int[,] num = new int[m, n];
+int min = num[0, 0];
+int [] num1 = new int[2];
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(1, 10);
+        Console.Write($"{num[i, j]} ");
+        if(num[i, j] < min) min = num[i, j]; 
+    }
+    Console.WriteLine();
+    
+}
