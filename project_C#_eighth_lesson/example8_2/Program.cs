b7bf9ba9b7f92c5c7da77ecb3ad4e76141c20fd2
Daniil Koblov <1koblov.danil1@gmail.com>
2022-09-17 22:23:55 +0300

    add example8_2

---
+++ project_C#_eighth_lesson/example8_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+Console.Clear();
+
+int m = new Random().Next(1, 4);
+int n = new Random().Next(1, 4);
+int[,] num = new int[m, n];
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(1, 10);
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Измененный массив.");
+
+int[,] array = new int[n, m];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = num[j, i];
+        Console.WriteLine($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
